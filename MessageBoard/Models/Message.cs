8f75db7d5e962dcb527d88afa6e16b5bfa2580a3
Louie Schoenknecht <luisesch97@gmail.com>
2021-03-29 11:40:18 -0700

    create initial project structure
    Co-Authored-By: Jeroen van Seeters <vanseetersjeroen@gmail.com>

---
+++ MessageBoard/Models/Message.cs
@@ -0,0 +1,9 @@
+namespace MessageBoard.Models
+{
+  public class Message
+  {
+    public int MessageId { get; set; }
+    public string Topic { get; set; }
+    public string Content { get; set; }
+  }
+}
